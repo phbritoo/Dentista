764f52ed4d8a97e282cebc22d63a57f6794880c7
Paulo Brito <paulohbsantana11@gmail.com>
2019-08-17 14:16:31 -0300

    Oraganizando a estrutura do projeto

--- dentista/src/app/app-routing.module.ts
+++ dentista/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
+import { IndexComponent } from './pages/index/index.component';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/index/index.component').then(m => m.IndexComponent)
+  }
 ];
-
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- dentista/src/app/app.module.ts
+++ dentista/src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { LoginComponent } from './pages/auth/login/login.component';
+import { HomeLaboratorioComponent } from './pages/home/home-laboratorio/home-laboratorio.component';
+import { HomeDentistaComponent } from './pages/home/home-dentista/home-dentista.component';
+import { IndexComponent } from './pages/index/index.component';
+import { CadastroLaboratorioComponent } from './pages/cadastro/cadastro-laboratorio/cadastro-laboratorio.component';
+import { CadastroDentistaComponent } from './pages/cadastro/cadastro-dentista/cadastro-dentista.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,10 +17,25 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './shared/material.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    CadastroDentistaComponent,
+    CadastroLaboratorioComponent,
+    IndexComponent,
+    HomeDentistaComponent,
+    HomeLaboratorioComponent,
+    LoginComponent
+  ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, MaterialModule, ReactiveFormsModule],
-  providers:[
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ],
+  providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
