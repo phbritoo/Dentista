8cf6b3d1356ac815255ce4e7c0b3c2fa53ea8d2b
Paulo Brito <paulohbsantana11@gmail.com>
2019-08-17 15:06:40 -0300

    ajeitando as rotas

--- dentista/src/app/app-routing.module.ts
+++ dentista/src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
+import { LoginComponent } from './pages/auth/login/login.component';
 import { IndexComponent } from './pages/index/index.component';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/index/index.component').then(m => m.IndexComponent)
-  }
+  { path: '', component: IndexComponent },
+  { path: 'login', component: LoginComponent }
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
