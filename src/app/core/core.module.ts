8a163feb35fb25f08d0ddf23b7fee9031f863d38
ediwaldoneto <ediwaldo.neto@outlook.com>
2019-09-01 18:34:37 -0300

    Config inicial firebase

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { RouteReuseStrategy } from '@angular/router';
+
 import { NgModule } from '@angular/core';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
+// Pacote do AngularFire
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { environment } from 'src/environments/environment';
+
 @NgModule({
-  imports: [IonicModule.forRoot()],
+  imports: [
+    IonicModule.forRoot(),
+    // Metodo do AngulaFire que fornece a inicializacao do projeto no firebase
+    // Esses servicos ficaram disponiveis por toda aplicacao
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
+  ],
   exports: [BrowserModule, IonicModule],
   providers: [
     StatusBar,
@@ -14,4 +27,4 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ]
 })
-export class CoreModule { }
+export class CoreModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase: {
+    apiKey: 'AIzaSyBOF4uvZ6NR1a_zgprT1wkFUrOIOHd0Dmk',
+    authDomain: 'protolab-284a7.firebaseapp.com',
+    databaseURL: 'https://protolab-284a7.firebaseio.com',
+    projectId: 'protolab-284a7',
+    storageBucket: '',
+    messagingSenderId: '995568254428',
+    appId: '1:995568254428:web:ba059ecfd6d1eed5'
+  }
 };
 
 /*
