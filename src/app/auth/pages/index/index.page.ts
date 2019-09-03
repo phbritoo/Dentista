4561788d255babba7d89f3f4a54c74e3cce79cf3
Gildo Neto <gildorama@gmail.com>
2019-09-03 20:07:57 -0300

    Início de desenvolvimento da tela index

--- src/app/auth/pages/index/index.page.ts
+++ src/app/auth/pages/index/index.page.ts
@@ -12,4 +12,19 @@ export class IndexPage implements OnInit {
   ngOnInit() {
   }
 
+  slides = [
+    {
+      img: 'assets/img/dentista.png',
+      titulo: 'ProtoLAB é essencial pra você<br>profissional da odontologia'
+    },
+    {
+      img: 'assets/img/protese.png',
+      titulo: 'Tenha o controle de todas as<br>próteses solicitadas'
+    },
+    {
+      img: 'assets/img/dente.png',
+      titulo: 'Desde o pedido até a entrega.<br>Faça já seu cadastro!'
+    }
+
+  ];
 }
