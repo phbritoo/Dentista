5870b6b34eba58a04001072883e5e7b8f10c8d0f
Gildo Neto <gildorama@gmail.com>
2019-09-10 21:00:58 -0300

    criando o cadastro-teste

---
+++ src/app/auth/pages/cadastro-teste/cadastro-teste.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-teste',
+  templateUrl: './cadastro-teste.page.html',
+  styleUrls: ['./cadastro-teste.page.scss'],
+})
+export class CadastroTestePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
