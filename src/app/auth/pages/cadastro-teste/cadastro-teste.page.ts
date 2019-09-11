aafa091f395952f6bbbb5b132face1fd2b8489b0
Gildo Neto <gildorama@gmail.com>
2019-09-10 21:33:10 -0300

    criando interface user

--- src/app/auth/pages/cadastro-teste/cadastro-teste.page.ts
+++ src/app/auth/pages/cadastro-teste/cadastro-teste.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { User } from 'src/app/shared/user';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-cadastro-teste',
@@ -7,7 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroTestePage implements OnInit {
 
-  constructor() { }
+  public userLogin: User = {};
+  public userRegister: User = {};
+  private loading: any;
+
+  constructor(
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private authService: AuthService,
+    private afs: AngularFirestore
+  ) { }
 
   ngOnInit() {
   }
