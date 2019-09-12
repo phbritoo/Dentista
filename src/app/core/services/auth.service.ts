abce4da07d3ea1272f9e18ef579941b67fe13e73
Gildo Neto <gildorama@gmail.com>
2019-09-12 20:08:45 -0300

    Criando os métodos do FireAuth em auth.service.ts

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
+import { User } from 'src/app/shared/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   // Propriedade afa criada no construtor que exporta do core.modules -> AngularFireAuthModeule o serviço AngularFireAuth
-  constructor(private afa: AngularFireAuth) {}
+  constructor(
+    private afa: AngularFireAuth
+  ) { }
 
   // Metodo de autenticacao para o login e passaword
-  private signInEmailPassword({ email, password }): Promise<auth.UserCredential> {
-    return this.afa.auth.signInWithEmailAndPassword(email, password);
+  login(user: User) {
+    return this.afa.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
+
+  register(user: User) {
+    return this.afa.auth.createUserWithEmailAndPassword(user.email, user.password);
+  }
+
+  logout() {
+    return this.afa.auth.signOut();
+  }
+
+  getAuth() {
+    return this.afa.auth;
   }
 }
