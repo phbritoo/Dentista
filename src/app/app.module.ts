264d1f4426478607013af8e13cbc8b24b08d552d
marcelloalbuca <marcello.albuca@gmail.com>
2019-09-12 21:31:37 -0300

    correção conflito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { environment } from 'src/environments/environment'
+import environment from 'src/environments/environment'
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -18,7 +18,7 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
     MaterialModule,
     ReactiveFormsModule,
     BrMaskerModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(environment),
     AngularFireAuthModule,
     AngularFirestoreModule
   ],
