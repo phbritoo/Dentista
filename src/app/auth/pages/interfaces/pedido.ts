91f6fb20e2b76332aa58a6166f3768ad1b61ee44
Gildo Neto <gildorama@gmail.com>
2019-09-22 09:21:17 -0300

    Método de delete pedido funcionando. Testado em produção

--- src/app/auth/pages/interfaces/pedido.ts
+++ src/app/auth/pages/interfaces/pedido.ts
@@ -1,5 +1,5 @@
 export interface Pedido {
-
+  id?: string;
   userId?: string;
   // nomeDentista?: string; // implementar quando conseguir dar o get do nome na collection
   emailProtetico?: string;
