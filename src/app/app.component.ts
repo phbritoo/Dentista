9618bef8c8fa31f2d523f2bed4ce854852aeddce
Paulo Brito <paulohbsantana11@gmail.com>
2019-09-28 14:41:52 -0300

    fazendo o menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, ViewChildren, QueryList, OnDestroy } from '@angular/core';
 
-import { Platform, IonRouterOutlet, AlertController } from '@ionic/angular';
+import { Platform, IonRouterOutlet, AlertController, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Router } from '@angular/router';
+import { Pages } from './auth/pages/interfaces/pages';
 
 @Component({
   selector: 'app-root',
@@ -14,15 +15,37 @@ export class AppComponent implements OnDestroy {
   backButtonSubscription;
   @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
 
-  public appPages = [];
+  public appPages: Array<Pages>;
 
   constructor(
     private platform: Platform,
     public router: Router,
     public alertController: AlertController,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public navCtrl: NavController
   ) {
+    this.appPages = [
+      {
+        title: 'Home',
+        url: '/home-dentista',
+        direct: 'root',
+        icon: 'home'
+      },
+      {
+        title: 'About',
+        url: '/novo-pedido',
+        direct: 'forward',
+        icon: 'information-circle-outline'
+      },
+
+      {
+        title: 'App Settings',
+        url: '/home-protetico',
+        direct: 'forward',
+        icon: 'cog'
+      }
+    ];
     this.initializeApp();
   }
 
@@ -74,5 +97,15 @@ export class AppComponent implements OnDestroy {
 
   ngOnDestroy() {
     this.backButtonSubscription.unsubscribe();
+  }
+
+  // vamos mudar
+
+  goToEditProgile() {
+    this.navCtrl.navigateForward('edit-profile');
+  }
+
+  logout() {
+    this.navCtrl.navigateRoot('/');
   }
 }
