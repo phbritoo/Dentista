b6f5352eee6ef189d2637fc9399a8ee3d7cf68f3
Paulo Brito <paulohbsantana11@gmail.com>
2019-10-17 22:38:24 -0300

    ajustes no editar pedido

--- src/app/auth/pages/editar-pedido/editar-pedido.module.ts
+++ src/app/auth/pages/editar-pedido/editar-pedido.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
-import { IonicModule } from '@ionic/angular';
-
 import { EditarPedidoPage } from './editar-pedido.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
   {
@@ -15,12 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
+  imports: [SharedModule, RouterModule.forChild(routes), NgbModule],
   declarations: [EditarPedidoPage]
 })
 export class EditarPedidoPageModule {}
