18c80f44ee50e002afd4490e353e85dc0a3046b3
Matheus Rhumenig <mrfmatheus@hotmail.com>
2019-10-19 11:31:00 -0300

    TELA de editar perfil (incompleta)

--- src/app/auth/pages/perfil-dentista/perfil-dentista.page.ts
+++ src/app/auth/pages/perfil-dentista/perfil-dentista.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil-dentista',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-dentista.page.scss'],
 })
 export class PerfilDentistaPage implements OnInit {
+  atualizarDentista: any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createForm();
+  }
+  ionViewDidLeave() {
+    this.createForm();
+  }
+  // validacoes do formulario
+  private createForm(): void {
+    this.atualizarDentista = this.fb.group({
+      telefone: ['', [Validators.required, Validators.minLength(15)]],
+      cpf: ['', [Validators.required, Validators.minLength(14)]],
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      data: ['', [Validators.required]],
+      cro: ['', [Validators.required, Validators.minLength(4)]],
+      criadoEm: [new Date().getTime()],
+      isDentista: [true]
+    });
   }
 
+  dateChanged(date) {
+    this.atualizarDentista.patchValue({
+      data: date.detail.value.substring(0, 10)
+    });
+  }
 }
