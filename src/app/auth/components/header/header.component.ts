0cefd487d6941ac0d5081a058aaff31305900422
Paulo Brito <paulohbsantana11@gmail.com>
2019-11-09 14:34:31 -0300

    implementando tela de protetico

--- src/app/auth/components/header/header.component.ts
+++ src/app/auth/components/header/header.component.ts
@@ -6,14 +6,12 @@ import { AuthService } from 'src/app/core/services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-   // ainda n ta funcionando
+  // ainda n ta funcionando
   loading: any;
-  constructor(
-              private authService: AuthService,
-              private loadingCtrl: LoadingController) { }
+  constructor(private authService: AuthService, private loadingCtrl: LoadingController) {}
 
   ngOnInit() {}
 
