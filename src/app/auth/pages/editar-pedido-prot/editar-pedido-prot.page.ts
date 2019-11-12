0b33f1b23bceaabf6f8d18e4cd4b6b6be2587626
Brito <paulo.brito@accenture.com>
2019-11-12 15:16:49 -0300

    implementando status em editar pedido prot

--- src/app/auth/pages/editar-pedido-prot/editar-pedido-prot.page.ts
+++ src/app/auth/pages/editar-pedido-prot/editar-pedido-prot.page.ts
@@ -10,16 +10,22 @@ import { Subscription, Observable } from 'rxjs';
 import { User } from '../interfaces/user';
 import { Categoria } from '../interfaces/categoria';
 import { Tipo } from '../interfaces/tipo';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-editar-pedido-prot',
   templateUrl: './editar-pedido-prot.page.html',
   styleUrls: ['./editar-pedido-prot.page.scss']
 })
-export class EditarPedidoProtPage implements OnInit {
+export class EditarPedidoProtPage implements OnInit, OnDestroy {
   editarPedido: FormGroup;
   public pedido: Pedido = {};
+  public pedidoClicado: any;
+  public mainpedido: AngularFirestoreDocument;
   private pedidoId: string = null;
   private loading: any;
   private pedidoSubscription: Subscription;
@@ -29,17 +35,14 @@ export class EditarPedidoProtPage implements OnInit {
   tipoCollection: AngularFirestoreCollection<Tipo>;
   categorias: Observable<Categoria[]>;
   tipos: Observable<Tipo[]>;
-
-  // titulo de cada select-option
-  proteticoLabel: any = {
-    header: 'Selecione o Protetico'
-  };
-  categoriaLabel: any = {
-    header: 'Categoria da Protese'
-  };
-  tipoLabel: any = {
-    header: 'Tipo de Protese'
-  };
+  sub;
+  nomeProtetico;
+  nomePaciente;
+  tipoProtese;
+  subTipoProtese;
+  criadoEm;
+  observacao;
+  status;
 
   constructor(
     private fb: FormBuilder,
@@ -56,42 +59,77 @@ export class EditarPedidoProtPage implements OnInit {
     if (this.pedidoId) {
       this.loadPedido();
     }
+    this.initializeApp();
   }
-  // validacoes do formulario
-  private createForm(): void {
-    this.editarPedido = this.fb.group({
-      idDentista: [this.authService.getAuth().currentUser.uid],
-      nomeProtetico: ['', [Validators.required]],
-      tipoProtese: ['', [Validators.required]],
-      subTipoProtese: ['', [Validators.required]],
-      picture: ['https://icon-library.net/images/tooth-icon-png/tooth-icon-png-28.jpg'],
-      observacao: ['', [Validators.required]],
-      status: ['PENDENTE'],
-      nomePaciente: ['', [Validators.required]],
-      criadoEm: [new Date().getTime()]
+
+  initializeApp() {
+    this.mainpedido = this.afs.doc('Pedidos/' + this.pedidoId);
+    this.sub = this.mainpedido.valueChanges().subscribe(event => {
+      this.nomeProtetico = event.nomeProtetico;
+      this.nomePaciente = event.nomePaciente;
+      this.tipoProtese = event.tipoProtese;
+      this.subTipoProtese = event.subTipoProtese;
+      this.criadoEm = event.criadoEm;
+      this.observacao = event.observacao;
+      this.status = event.status;
     });
   }
 
   ngOnInit(): void {
     this.createForm();
-    this.userCollection = this.afs.collection('User');
-    this.users = this.userCollection.valueChanges();
-    this.categoriaCollection = this.afs.collection('Categoria');
-    this.categorias = this.categoriaCollection.valueChanges();
-    this.tipoCollection = this.afs.collection('Tipo');
-    this.tipos = this.tipoCollection.valueChanges();
-
-    // funcao para o botao enivar sumir, funcao esta no css
-    document.getElementById('editar').classList.add('aparecer');
-    document.getElementById('editar').classList.add('ion-margin');
-
-    this.editarPedido.disable();
+    this.editarPedido.controls.nomeProtetico.disable();
+    this.editarPedido.controls.tipoProtese.disable();
+    this.editarPedido.controls.subTipoProtese.disable();
   }
-
   ngOnDestroy() {
     if (this.pedidoSubscription) {
       this.pedidoSubscription.unsubscribe();
     }
+  }
+  ionViewDidLeave() {
+    this.createForm();
+  }
+
+  // validacoes do formulario
+  private createForm(): void {
+    this.editarPedido = this.fb.group({
+      nomeProtetico: ['', [Validators.required]],
+      nomePaciente: ['', [Validators.required]],
+      tipoProtese: ['', [Validators.required]],
+      subTipoProtese: ['', [Validators.required]],
+      observacao: ['', [Validators.required]],
+      status: ['', [Validators.required]],
+      criadoEm: ['']
+    });
+  }
+
+  // mostrar mensagem de erro no ion-note
+  get nomeProt(): FormControl {
+    return this.editarPedido.get('nomeProtetico') as FormControl;
+  }
+  // mostrar mensagem de erro no ion-note
+  get nomePac(): FormControl {
+    return this.editarPedido.get('nomePaciente') as FormControl;
+  }
+  // mostrar mensagem de erro no ion-note
+  get tipo(): FormControl {
+    return this.editarPedido.get('tipoProtese') as FormControl;
+  }
+  // mostrar mensagem de erro no ion-note
+  get subTipo(): FormControl {
+    return this.editarPedido.get('subTipoProtese') as FormControl;
+  }
+  // mostrar mensagem de erro no ion-note
+  get obs(): FormControl {
+    return this.editarPedido.get('observacao') as FormControl;
+  }
+  // mostrar mensagem de erro no ion-note
+  get stat(): FormControl {
+    return this.editarPedido.get('status') as FormControl;
+  }
+  // mostrar mensagem de erro no ion-note
+  get criacao(): FormControl {
+    return this.editarPedido.get('criadoEm') as FormControl;
   }
 
   loadPedido() {
@@ -101,33 +139,35 @@ export class EditarPedidoProtPage implements OnInit {
   }
 
   async atualizarPedido() {
-    await this.presentLoading();
-    // pegando o id do usuário
-    this.pedido.idDentista = this.authService.getAuth().currentUser.uid;
-    // se já existir produto, atualiza. Se não existe, cria um novo produto.
-    if (this.pedidoId) {
-      try {
-        await this.pedidoService.updatePedido(this.pedidoId, this.editarPedido.value);
-        await this.loading.dismiss();
-
-        this.navCtrl.navigateBack('/home-protetico');
-      } catch (error) {
-        this.presentToast('Erro ao tentar salvar');
-        this.loading.dismiss();
-      }
-    } else {
-      this.pedido.criadoEm = new Date().getTime();
-
-      try {
-        await this.pedidoService.addPedido(this.editarPedido.value);
-        this.presentToast('<center>' + 'Pedido encaminhado ao Protetico ' + '</center>');
-        await this.loading.dismiss();
-        this.navCtrl.navigateBack('/home-dentista');
-      } catch (error) {
-        this.presentToast('Erro ao tentar salvar');
-        this.loading.dismiss();
-      }
+    if (!this.nomePaciente) {
+      return this.presentToast('<center>' + 'Informe o nome do paciente !' + '</center>');
     }
+    if (!this.tipoProtese) {
+      return this.presentToast('<center>' + 'Informe o tipo da Prótese !' + '</center>');
+    }
+    if (!this.subTipoProtese) {
+      return this.presentToast('<center>' + 'Informe o subtipo da Prótese !' + '</center>');
+    }
+    if (!this.observacao) {
+      return this.presentToast('<center>' + 'Escreva observações importantes !' + '</center>');
+    }
+    if (!this.status) {
+      return this.presentToast('<center>' + 'Escolha o Status !' + '</center>');
+    }
+    try {
+      await this.mainpedido.update({
+        nomePaciente: this.nomePaciente,
+        tipoProtese: this.tipoProtese,
+        subTipoProtese: this.subTipoProtese,
+        observacao: this.observacao,
+        status: this.status
+      });
+    } catch (error) {
+      return await this.presentToast('<center>' + 'Error ao atualizar !' + '</center>');
+    }
+    this.presentToast('<center>' + 'Pedido atualizado com sucesso !' + '</center>');
+    // this.router.navigateBack(['/home-dentista']);
+    this.navCtrl.navigateBack(['/home-protetico']);
   }
 
   async presentLoading() {
@@ -142,15 +182,6 @@ export class EditarPedidoProtPage implements OnInit {
 
   cancelar() {
     this.router.navigate(['/home-protetico']);
-    this.editarPedido.disable();
-  }
-  editar() {
-    this.editarPedido.enable();
-    //  this.editarPedido.controls.nomePaciente.enable();
-
-    // funcao para o botao enivar sumir, funcao esta no css
-    document.getElementById('enviar').classList.add('aparecer');
-    document.getElementById('enviar').classList.add('ion-margin');
-    document.getElementById('editar').classList.remove('aparecer');
+    // this.editarPedido.disable();
   }
 }
